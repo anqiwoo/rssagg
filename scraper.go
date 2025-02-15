99d16f29997425b1791056ab9081c5a70eaabe7a
Anqi Wu <anqiwu85@outlook.com>
2025-02-15 07:17:35 -0800

    Add CreatePost logic

--- scraper.go
+++ scraper.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"github.com/anqiwoo/rssagg/internal/database"
+	"github.com/google/uuid"
 	"log"
+	"strings"
 	"sync"
 	"time"
 )
@@ -48,7 +51,35 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		publishedAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing published date %v: %s", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: publishedAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			// Skip expected errors for parsed posts
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Printf("Error creating post: %s", err)
+		}
 	}
 	log.Printf("Finished scraping feed %s, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
